0ed6c72b1414f4b4db3e74d5a75da5c70bca3380
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-03-31 08:02:36 +0100

    reformatting

--- src/functions/chatbox.js
+++ src/functions/chatbox.js
@@ -1,27 +1,16 @@
-// Load the dotenv package to use environmental variables
-require('dotenv').config();
-
-// Load the app module from Azure Functions library
-const { app } = require('@azure/functions');
-
-// Load the application module from service folder
+const { app: { http } } = require('@azure/functions');
 const application = require('../../service/app2');
-
-// Load and configure the TelegramBot using a BOT_TOKEN environment variable
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(process.env.BOT_TOKEN);
-bot.setWebHook(process.env.BOT_WEBHOOK);
 
-// Handles HTTP requests for the 'chatbox' endpoint with anonymous authentication level
-app.http('chatbox', {
+http('chatbox', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
-    handler: async (request, context) => {
-        // Call the application module function and pass in context, request, chat database name,
-        // chat history container name, and the TelegramBot object
-        let response = await application(context, request, 'ChatDB', 'chatHistory', bot)
+    handler: async (context, req) => {
+        const bot = new TelegramBot(`${process.env.BOT_TOKEN}`);
+        bot.setWebHook(`${process.env.BOT_WEBHOOK}`);
 
-        // Return the response as the body of the HTTP response
-        return { body: response }
+        const response = await application(context, req, 'ChatDB', 'chatHistory', bot);
+
+        return { body: response };
     }
 });
--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -1,15 +1,14 @@
-const { app } = require('@azure/functions');
+const { app: { http } } = require('@azure/functions');
 const application = require('../../service/app2')
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot('6289884430:AAEczOn9ztfxs-Q8hZQbEAY_N0zQZJqI82w');
-bot.setWebHook('https://chat-assist-bot.azurewebsites.net/api/httptrigger1');
 
-app.http('httpTrigger1', {
+http('httpTrigger1', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
-    handler: async (request, context) => {
-        let response = await application(context, request, 'ChatDB', 'chatHistoryBot', bot)
+    handler: async (context, req) => {
+        const bot = new TelegramBot('TOKEN_HERE');
+        bot.setWebHook('https://chat-assist-bot.azurewebsites.net/api/httptrigger1');
+        const response = await application(context, req, 'ChatDB', 'chatHistoryBot', bot)
         return { body: response }
     }
 });
-
