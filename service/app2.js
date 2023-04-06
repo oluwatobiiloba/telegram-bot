619533a64a7f6a14d3461d9df06a5288b58c2e85
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-04-06 01:11:22 +0100

    refactor

--- service/app2.js
+++ service/app2.js
@@ -1,234 +1,61 @@
 const axios = require('axios');
 const cosmo_surfer = require('./cosmo_util');
 const spotify_helper = require('./spotify_helper');
-const { PDFDocument } = require('pdf-lib');
-const pdfParse = require('pdf-parse');
-const { BlobServiceClient } = require("@azure/storage-blob")
 const openAI_API = axios.create({
     baseURL: process.env.OPEN_AI_URL,
     headers: { Authorization: `Bearer ${process.env.OPEN_AI_TOKEN}` },
 });
+const document_processing = require('./document_processing');
+const help_text = require('../bot_helpers');
 
-const { jsPDF } = require('jspdf');
 
 module.exports = async function (context, request, database, container, bot) {
-    const body = await request.json();
-    context.log('Request body', body);
-
-    context.log('Request body', body);
-    //Check for bot kick event and video_note messages
-    if (body.my_chat_member) {
-        bot.sendMessage(body.my_chat_member.chat.id, '🤖');
-        context.log('User kicked me out of the group');
-        return {
-            status: 200,
-            body: 'User kicked me out of the group',
-        };
-    } else if (body.message.video_note) {
-        context.log('Video note received');
-        bot.sendMessage(body.message.chat.id, "I'm incapable of processing video notes.");
-        return {
-            status: 200,
-            body: "Video note received, but I'm incapable of processing video notes.",
-        };
-    } else if (body.message.document && body.message.document.mime_type === 'application/pdf' && body.message.document.file_name.includes("resume")) {
-        context.log('resume received');
-        bot.sendMessage(body.message.chat.id, "On it!");
-        try {
-            const chatId = body.message.chat.id;
-            const fileId = body.message.document.file_id;
-            const fileName = body.message.document.file_name;
-
-            const fileUrl = await bot.getFileLink(fileId);
-            const fileResponse = await axios.get(fileUrl, { responseType: 'arraybuffer' });
-
-            const text = await pdfParse(fileResponse.data);
-
-            const openaiPromptMessage = `Improve the resume below. Add a professional summary that emphasizes the skills relevant to the educational background, role, and experience. Highlight. Ensure your response does not contain characters that cannot be encoded by common text encodings. ${text.text}`;
-            const cleanText = openaiPromptMessage.replace(/[^\x00-\x7F]/g, '-').replace(/[\u2022-\u2027\u25AA-\u25FF]/g, '-');
-
-            const file_response = await openAI_API.post('/chat/completions', {
-                model: 'gpt-3.5-turbo',
-                messages: [{ role: 'user', content: cleanText }],
-            });
-
-
-            bot.sendMessage(chatId, "I'm writing your resume!🤖🤖");
-
-            let resume = file_response.data.choices[0].message.content;
-            resume = `${resume}`
-            const final_resume = resume.replace(/""/g, "");
-
-            const doc = new jsPDF();
-            doc.setFontSize(12);
-            doc.setLineHeightFactor(1.2);
-
-            // split the text into an array of lines
-            const textLines = doc.splitTextToSize(final_resume, doc.internal.pageSize.width - 20);
-
-            // loop over the lines and add them to the PDF
-            let y = 10;
-            for (let i = 0; i < textLines.length; i++) {
-                // calculate the y position based on the current page
-                const pageHeight = doc.internal.pageSize.height - 20;
-                const contentHeight = doc.getTextDimensions(textLines[i]).h;
-                const remainingHeight = pageHeight - y;
-                if (contentHeight > remainingHeight) {
-                    doc.addPage();
-                    y = 10;
-                }
-                y += doc.getLineHeightFactor();
-                if (y > pageHeight) {
-                    doc.addPage();
-                    y = 10;
-                }
-                doc.text(textLines[i], 10, y);
-                y += contentHeight + doc.getLineHeightFactor();
-            }
-
-            const modifiedPdfBytes = doc.output('arraybuffer');
-
-            const containerName = 'pdf'
-            const blobName = `${fileName}${fileId}${chatId}.${fileName.split('.').pop()}`
-            const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_ACCOUNT_NAME);
-            const containerClient = blobServiceClient.getContainerClient(containerName);
-            const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
-            await blockBlobClient.upload(modifiedPdfBytes, modifiedPdfBytes.byteLength, {
-                blobHTTPHeaders: {
-                    blobContentType: 'application/pdf'
-                },
-                fileName: `${fileName}${fileId}${chatId}.${fileName.split('.').pop()}`
-            });
-
-            await bot.sendMessage(chatId, `Here's your improved resume! : ${blockBlobClient.url}`);
-            await bot.sendMessage(chatId, `Hope it helps!`);
-        } catch (error) {
-            context.error("Error occurred:", error);
-            return {
-                status: 500,
-                body: `Document received, but An internal error occurred while processing the request. ${error}`,
-            };
-        }
-
-        return {
-            status: 500,
-            body: "Document received, but I'm incapable of processing documents.",
-        };
-    } else if (body.message.document && body.message.document.mime_type === 'application/pdf') {
-        context.log('Document received');
-        bot.sendMessage(body.message.chat.id, "On it!");
-
-        try {
-            const chatId = body.message.chat.id;
-            const fileId = body.message.document.file_id;
-            const fileName = body.message.document.file_name;
-
-            const fileUrl = await bot.getFileLink(fileId);
-            const fileResponse = await axios.get(fileUrl, { responseType: 'arraybuffer' });
-
-            const text = await pdfParse(fileResponse.data);
-
-            const openaiPromptMessage = `Improve the writeup below. Ensure your response does not contain characters that cannot be encoded by common text encodings.  ${text.text}`
-            const cleanText = openaiPromptMessage.replace(/[^\x00-\x7F]/g, '-').replace(/[\u2022-\u2027\u25AA-\u25FF]/g, '-');
-
-            const file_response = await openAI_API.post('/chat/completions', {
-                model: 'gpt-3.5-turbo',
-                messages: [{ role: 'user', content: cleanText }],
-            });
-
-
-            bot.sendMessage(chatId, "I'm writing your resume!🤖🤖");
-
-            let resume = file_response.data.choices[0].message.content;
-            resume = `${resume}`
-            const final_resume = resume.replace(/""/g, "");
-
-            const doc = new jsPDF();
-            doc.setFontSize(12);
-            doc.setLineHeightFactor(1.2);
-
-            // split the text into an array of lines
-            const textLines = doc.splitTextToSize(final_resume, doc.internal.pageSize.width - 20);
-
-            // loop over the lines and add them to the PDF
-            let y = 10;
-            for (let i = 0; i < textLines.length; i++) {
-                // calculate the y position based on the current page
-                const pageHeight = doc.internal.pageSize.height - 20;
-                const contentHeight = doc.getTextDimensions(textLines[i]).h;
-                const remainingHeight = pageHeight - y;
-                if (contentHeight > remainingHeight) {
-                    doc.addPage();
-                    y = 10;
-                }
-                y += doc.getLineHeightFactor();
-                if (y > pageHeight) {
-                    doc.addPage();
-                    y = 10;
-                }
-                doc.text(textLines[i], 10, y);
-                y += contentHeight + doc.getLineHeightFactor();
-            }
-
-            const modifiedPdfBytes = doc.output('arraybuffer');
-
-            const containerName = 'pdf'
-            const blobName = `${fileName}${fileId}${chatId}.${fileName.split('.').pop()}`
-            const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_ACCOUNT_NAME);
-            const containerClient = blobServiceClient.getContainerClient(containerName);
-            const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
-            await blockBlobClient.upload(modifiedPdfBytes, modifiedPdfBytes.byteLength, {
-                blobHTTPHeaders: {
-                    blobContentType: 'application/pdf'
-                },
-                fileName: `${fileName}${fileId}${chatId}.${fileName.split('.').pop()}`
-            });
-
-            await bot.sendMessage(chatId, `Here's your improved Document! : ${blockBlobClient.url}`);
-            await bot.sendMessage(chatId, `Hope it helps!`);
-        } catch (error) {
-            context.error("Error occurred:", error);
-            return {
-                status: 500,
-                body: `Document received, but An internal error occurred while processing the request. ${error}`,
-            };
-        }
-        return {
-            status: 200,
-            body: "Document received, but an error occured while processing",
-        };
-    }
-
-
-  //retrieve chat ID
-    const { chat: { id: chatId } } = body.message;
 
     try {
+        const body = await request.json();
+        context.log('Request body', body);
+        const is_resume = body.message?.document && body.message?.document.mime_type === 'application/pdf' && body.message?.document?.file_name.toLowerCase().includes("resume")
+        const is_document = body.message?.document && body.message?.document.mime_type === 'application/pdf' && !body.message?.document?.file_name.toLowerCase().includes("resume")
+
+        const { chat: { id: chatId } } = body.message;
+        if (body.my_chat_member) {
+            bot.sendMessage(body.my_chat_member.chat.id, '🤖');
+            context.log('User kicked me out of the group');
+            return {
+                status: 200,
+                body: 'User kicked me out of the group',
+            };
+        } else if (body.message.video_note) {
+            context.log('Video note received');
+            bot.sendMessage(body.message.chat.id, "I'm incapable of processing video notes.");
+            return {
+                status: 200,
+                body: "Video note received, but I'm incapable of processing video notes.",
+            };
+        }
         //retrieve chat history
         let messages = await cosmo_surfer.getChatHistory(chatId, database, container);
         //add new message to chat history
         messages.push({ role: 'user', content: body.message.text });
         //retrieve prompt for non-conversation messages
-        const prompt_req = body.message.text.toLowerCase();
-        let imageUrls = [];
+        const prompt_req = body.message?.text?.toLowerCase() || "null";
+
 
         switch (true) {
             case prompt_req === '/clear':
                 //clear chat history
                 context.log('Clearing chat history');
                 await cosmo_surfer.deleteChatHistory(chatId, database, container);
-                await bot.sendMessage(chatId, 'Chat history cleared');
+                await bot.sendMessage(chatId, help_text.clear_history);
                 return {
                     status: 200,
-                    message: 'Chat history cleared',
+                    message: help_text.clear_history,
                 };
             case prompt_req === '/start':
                 //start conversation
                 await bot.sendMessage(
                     chatId,
-                    `Hi, I'm Chat Assist Bot. I can help you with your daily tasks. You can ask me to create an image, search for a movie, or even get the weather forecast. I can also help you with your homework. Just reply with /help to see all the commands I can do. Please note that chats are stored until you clear them using /clear command.`,
+                    help_text.start,
                 );
                 return {
                     status: 200,
@@ -238,7 +65,7 @@ module.exports = async function (context, request, database, container, bot) {
                 //help message
                 await bot.sendMessage(
                     chatId,
-                    `Hi, simply ask any questions and I will try to answer them. You can also ask me to create an image by asking me to create an image of a <your image prompt here>. You can also generate a spotify playlist by asking me to 'Create a playlist, Mood - <describe the context or your mood>. Preferred Artist -  <describe the preferred artists or genres>. If I don't respond, please send /clear to clear the chat history and start again. `,
+                    help_text.help,
                 );
                 return {
                     status: 200,
@@ -253,7 +80,7 @@ module.exports = async function (context, request, database, container, bot) {
                     n: 2,
                     size: '1024x1024',
                 });
-                imageUrls = imageResponse.data.data;
+                let imageUrls = imageResponse.data.data || [];
                 //create media group
                 const media = imageUrls.map((url) => ({
                     type: 'photo',
@@ -283,11 +110,12 @@ module.exports = async function (context, request, database, container, bot) {
                     messages: [{ role: 'user', content: playlistMessage }],
                 }).catch(async (err) => {
                     context.log('Error getting response from OpenAi', err);
-                    await bot.sendMessage(chatId, "I'm sorry, I ruined your playlist 😢. Please try again in a minute while I gather my thoughts.");
+                    await bot.sendMessage(chatId, "I'm sorry, I ruined your playlist 😢. Please try again in a minute while I gather my thoughts 😿.");
                 });
                 await bot.sendMessage(chatId, 'Still working on it..., my creator is still working on making me faster 🤖. In the meantime,  take me as I am 🤗.');
                 let songs = playlist_response.data.choices[0].message.content;
                 let spotify_query = null
+                context.log('Songs', songs);
                 try {
                     spotify_query = JSON.parse(songs.substring(songs.indexOf('['), songs.lastIndexOf(']') + 1))
                 } catch (error) {
@@ -324,8 +152,61 @@ module.exports = async function (context, request, database, container, bot) {
                     return {
                         status: 200,
                         message: 'No songs recommended.',
+                    }
+                };
+            case is_resume:
+                context.log('Resume received');
+
+                try {
+                    await bot.sendMessage(body.message.chat.id, "I'm on it!");
+                    const extracted_text = await document_processing.retrieve_document(context, body, bot, true)
+                    const fileName = extracted_text.fileName;
+                    const chatId = extracted_text.chatId;
+                    const openai_prompt = await document_processing.openai_prompt(context, extracted_text.text, bot, true, chatId)
+                    let resume = `${openai_prompt.data.choices[0].message.content}`;
+                    resume = resume.replace(/""/g, "");
+                    const pdf = await document_processing.create_pdf(context, resume, bot, chatId)
+                    const upload_to_blob = await document_processing.upload_to_blob(context, pdf, fileName, bot, chatId)
+                    await bot.sendMessage(chatId, `Here's your improved resume! : ${upload_to_blob}`);
+                    await bot.sendMessage(chatId, `Hope it helps!`);
+                    return {
+                        status: 200,
+                        body: "Resume received, and processed.",
+                    };
+                } catch (error) {
+                    context.error("Error occurred:", error);
+                    return {
+                        status: 500,
+                        body: `Document received, but An internal error occurred while processing the request. ${error}`,
                     };
                 }
+            case is_document:
+                context.log('Document received');
+
+                try {
+                    await bot.sendMessage(body.message.chat.id, "I'm on it!");
+                    const extracted_text = await document_processing.retrieve_document(context, body, bot, false)
+                    const fileName = extracted_text.fileName;
+                    const chatId = extracted_text.chatId;
+                    const openai_prompt = await document_processing.openai_prompt(context, extracted_text.text, bot, false, chatId)
+                    let document = `${openai_prompt.data.choices[0].message.content}`;
+                    document = document.replace(/""/g, "");
+                    const pdf = await document_processing.create_pdf(context, document, bot, chatId)
+                    const upload_to_blob = await document_processing.upload_to_blob(context, pdf, fileName, bot, chatId)
+                    await bot.sendMessage(chatId, `Here's your improved document! : ${upload_to_blob}`);
+                    await bot.sendMessage(chatId, `Hope it helps!`);
+                    return {
+                        status: 200,
+                        body: "Resume received, and processed.",
+                    };
+                } catch (error) {
+                    context.error("Error occurred:", error);
+                    return {
+                        status: 500,
+                        body: `Document received, but An internal error occurred while processing the request. ${error}`,
+                    };
+                }
+
             default:
                 const history = messages;
                 if (messages?.length > 7) messages.shift();
