49fad82d794b408cef493b88dd5364ea0572c347
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-04-09 17:57:49 +0100

    optimize implementation

--- src/functions/chatbox.js
+++ src/functions/chatbox.js
@@ -1,26 +1,27 @@
 require('dotenv').config();
-const { app } = require('@azure/functions');
-const application = require('../../service/app2')
+const { app } = require('@azure/functions'),
+    application = require('../../service/app2');
 const TelegramBot = require('node-telegram-bot-api');
-const regex = /[A-Za-z0-9_-]{22,}/;
 const { Queue } = require('bullmq');
+const regex = /[A-Za-z0-9_-]{22,}/;
+
+const { BOT_TOKEN, BOT_WEBHOOK, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
+const bot = new TelegramBot(BOT_TOKEN);
+bot.setWebHook(BOT_WEBHOOK, {
+    drop_pending_updates: true
+});
 const queue = new Queue('chatbox', {
     connection: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
-        password: process.env.REDIS_PASSWORD
+        host: REDIS_HOST,
+        port: REDIS_PORT,
+        password: REDIS_PASSWORD
     }
 });
-
 
 app.http('chatbox', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        const bot = new TelegramBot(process.env.BOT_TOKEN);
-        bot.setWebHook(process.env.BOT_WEBHOOK, {
-            drop_pending_updates: true
-        });
         const body = await request.json();
         const prompt_req_doc = body.message?.text || null
         const contains_id = prompt_req_doc?.match(regex) ? true : false
