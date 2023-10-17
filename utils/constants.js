c052b5ea5d0e8e133f46906a46059306ec7089a7
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-10-17 17:26:21 +0100

    update regex

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
   DOC_REGEX: /^\/(?:resume|document) [a-z0-9]{32}/g,
   APPLE_REGEX: /^https:\/\/music\.apple\.com\/\S+/gi,
-  YOUTUBE_REGEX: /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(watch\?v=|embed\/|v\/|watch\?feature=player_embedded&v=|watch\?feature=player_embedded&v=|youtu.be\/|user\/\S+\/|playlist\?list=)([\w-]+)/,
+  YOUTUBE_REGEX: /(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=|embed\/|v\/|watch\?feature=player_embedded&v=|watch\?feature=player_embedded&v=|user\/\S+\/|playlist\?list=)?([\w-]+)(?:\S*)?/,
   CHATBOX_QUEUE_NAME: 'chatbox',
   JOB_PROCESS_DOC: 'process-document',
   NO_DB_RESOURCE_FOUND: 'No resource found!',
