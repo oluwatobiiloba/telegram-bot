fea89504bcda20cbb5dfe771255c076a621979dd
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-10-17 20:54:42 +0100

    use buffer instead

--- services/request-handlers/convert-download-youtube-video.js
+++ services/request-handlers/convert-download-youtube-video.js
@@ -1,11 +1,10 @@
-const ytdl = require("ytdl-core")
+const ytdl = require("ytdl-core");
 const TimeLogger = require("../../utils/timelogger");
 const { staticBotMsgs, logMsgs, dynamicBotMsgs } = require("../../messages");
 const resUtil = require("../../utils/res-util");
-const fs = require("fs");
-const path = require("path");
+
 async function handler({ prompt, chatId, bot, body }) {
-    const isValidUrl = await ytdl.validateURL(prompt)
+    const isValidUrl = await ytdl.validateURL(prompt);
     const timeLogger = new TimeLogger(`CONVERT-YOUTUBLE-URL-DURATION-${Date.now()}`);
 
     try {
@@ -16,41 +15,44 @@ async function handler({ prompt, chatId, bot, body }) {
             const videoInfo = await ytdl.getInfo(prompt);
             timeLogger.end("getting-video-details");
             await bot.sendMessage(chatId, staticBotMsgs.DOWNLOAD_YOUTUBE_SEQ[1] + videoInfo.videoDetails.title);
-            const tempFilePath = path.join(__dirname, `temp_${Date.now()}.mp4`);
-            const videoFileStream = fs.createWriteStream(tempFilePath);
-            ytdl(prompt).pipe(videoFileStream);
-            await new Promise((resolve, reject) => {
-                videoFileStream.on('finish', resolve);
-                videoFileStream.on('error', reject);
-            });
-            const videoReadStream = fs.createReadStream(tempFilePath);
-              await bot.sendMessage(chatId, staticBotMsgs.DOWNLOAD_YOUTUBE_SEQ[2] + videoInfo.videoDetails.title);
-           timeLogger.start("sending-video");
-           await bot.sendVideo(chatId, videoReadStream, {}, {
-               filename: videoInfo.title
-           });
-            timeLogger.end("sending-video");
-            funcResponse = resUtil.success({
-                message: logMsgs.VIDEO_DOWNLOADED,
-                data: videoInfo.videoDetails.title,
+
+            const videoStream = ytdl(prompt, { quality: 'highest' });
+            const chunks = [];
+            
+            videoStream.on('data', (chunk) => {
+                chunks.push(chunk);
             });
 
-          //  fs.unlinkSync(tempFilePath);
-            return funcResponse
+            videoStream.on('end', async () => {
+                const videoBuffer = Buffer.concat(chunks);
+                await bot.sendMessage(chatId, staticBotMsgs.DOWNLOAD_YOUTUBE_SEQ[2] + videoInfo.videoDetails.title);
+                timeLogger.start("sending-video");
+                await bot.sendVideo(chatId, videoBuffer, {}, {
+                    filename: videoInfo.videoDetails.title + ".mp4",
+                });
+                timeLogger.end("sending-video");
+
+                funcResponse = resUtil.success({
+                    message: logMsgs.VIDEO_DOWNLOADED,
+                    data: videoInfo.videoDetails.title,
+                });
+            });
+
+            videoStream.on('error', (error) => {
+                throw error;
+            });
         } else {
             await bot.sendMessage(chatId, staticBotMsgs.ERROR_CON_YOUTUBE_VIDEO);
             funcResponse = resUtil.success(logMsgs.NO_VIDEO_CONVERTED);
         }
-        return funcResponse
+        return funcResponse;
     } catch (err) {
         await bot.sendMessage(chatId, staticBotMsgs.ERROR_CON_YOUTUBE_VIDEO);
         err.message = `CONVERT-YOUTUBE-VIDEO-REQ-HANDLER: ${err.message}`;
-        throw err
+        throw err;
     } finally {
-       timeLogger.log();
+        timeLogger.log();
     }
-
 }
 
-
-module.exports = handler
+module.exports = handler;
